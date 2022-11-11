48b081c02191b38f09e81f478dd3dc860418c254
Brian Leguizamon <104389659+Thundeck@users.noreply.github.com>
2022-11-11 12:47:28 -0300

    Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import Landing from './components/Landing';
+import Home from './components/Home';
+import CreatePokemon from './components/CreatePokemon';
+import SearchBar from './components/Searchbar';
+import PokemonDetails from './components/PokemonDetails';
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
+    <div >
+      <SearchBar/>
+      <Route exact path='/' component={Landing}/>
+      <Route exact path='/home' component={Home}/>
+      <Route exact path='/home/:id'component={PokemonDetails}/>
+      <Route exact path='/home/create' component={CreatePokemon}/>
     </div>
   );
 }
---
+++ client/src/components/Landing/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+        <link to='/home'>esto no se ve</link>
+    </div>
+  )
+}
+
+export default Landing
